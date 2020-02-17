619b6a5ba7c7d6f52cc7688fe86d6e70e4c06339
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-02-17 02:46:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,12 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0,8)
     true
   end
 end
-
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
@@ -20,12 +23,6 @@ def position_taken?(board, index)
     true
   end
 end
-
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
   
 def move(board, index, character="X")
   board[index] = character
